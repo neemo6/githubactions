38e326d2b6acc9bab97251f40f72c9b51852639b
neemo6 <neeleshwrk@gmail.com>
2026-07-03 17:35:41 +0530

    Update backend.tf for s3

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "neelesh-terraform-state-2026"
+    bucket         = "neelesh-terr-state-2026"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "neelesh-terraform-lock"
