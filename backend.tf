11033e9d29806122e4bef94c0125936775c3974a
neemo6 <neeleshwrk@gmail.com>
2026-07-06 12:11:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "neelesh-terr-state-2026"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "neelesh-terraform-lock"
-    encrypt        = true
+    bucket       = "neelesh-terr-state-2026"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"       # use your actual region
+    use_lockfile = true
+    encrypt      = true
   }
 }
