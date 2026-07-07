dcabad69dad0b132d2fe531480480e29dc964857
neemo6 <neeleshwrk@gmail.com>
2026-07-07 11:39:15 +0530

    Update main.tf with ami-06067086cf86c58e6 and t2.micro

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
+provider "aws" {
+}
+
 resource "aws_instance" "dev" {
   ami           = "ami-06067086cf86c58e6"
-  instance_type = "t2.medium"
+  instance_type = "t2.micro"
+
   tags = {
     Name = "Neelesh"
   }
 }
-
