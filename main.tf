032db05c0a2767063240abe59aed224ac4d384bf
neemo6 <neeleshwrk@gmail.com>
2026-07-07 11:13:22 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
-provider "aws" {
-  
+resource "aws_instance" "dev" {
+  ami           = "ami-06067086cf86c58e6"
+  instance_type = "t2.medium"
+  tags = {
+    Name = "Neelesh"
+  }
 }
 
-resource "aws_instance" "dev" {
-    ami = "ami-0f88e80871fd81e91"
-    instance_type = "t2.micro"
-    tags = {
-      Name = "Neelesh"
-    }
-}
